f26ee50e4bf66565a266e6d7918184f37d55a8f5
Kenny Egun <kennyegun240@gmail.com>
2023-05-30 11:51:11 +0100

    made requested changes

--- src/components/homeComps/PostUpdate.jsx
+++ src/components/homeComps/PostUpdate.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import vic from '../../images/vic.jpg'
-import { FaCamera, FaPen } from 'react-icons/fa'
+import { FaBell, FaCamera, FaCheck, FaCheckCircle, FaPen } from 'react-icons/fa'
 import './postUpdate.css'
 
 const PostUpdate = () => {
+    const [postActive, setPostActive] = useState(false)
+
+    const textareaRef = useRef(null);
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (textareaRef.current && !textareaRef.current.contains(event.target)) {
+                setPostActive(false);
+            }
+        };
+
+        document.addEventListener('click', handleClickOutside);
+
+        return () => {
+            document.removeEventListener('click', handleClickOutside);
+        };
+    }, []);
+
     return (
         <div className='postUpdateDiv' >
             <div className='postUpdateDivSm' >
@@ -18,15 +36,28 @@ const PostUpdate = () => {
                 <div className='postUpdateFormDiv' >
                     <img className='postUpdateFormImage' src={vic} alt="" />
                     <form className='width100' >
-                        <textarea className='textarea' placeholder='Post anything...' rows='7' />
+                        <textarea ref={textareaRef} onClick={() => setPostActive(true)} className='textarea' placeholder='Post something about you...' rows='7' />
                     </form>
                 </div>
-                <div className='postUpdateBottomNavDiv' >
+                <div className='postUpdateBottomNavDiv padding1rem' >
                     <div className='postUpdateBottomNav' >
                         <FaCamera />
                         <p className='font12' >media</p>
                     </div>
                 </div>
+                {
+                    postActive &&
+                    <>
+                        <div className='flex align_center gap1rem padding1rem postActivityDiv'>
+                            <FaCheckCircle className='postActivityIcon' />
+                            <FaBell className='postActivityIcon' />
+                            <p className='white'>Activity Feed</p>
+                        </div>
+                        <div className='postUpdateButtonNavDiv' >
+                            <button className='pointer postUpdateButton text_center' >Post</button>
+                        </div>
+                    </>
+                }
             </div>
         </div>
     )
