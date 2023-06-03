d1bef0e01fb423ca3f4282552d82280f0a19710a
Kenny Egun <kennyegun240@gmail.com>
2023-06-03 11:15:45 +0100

    Enable image upload and resized images before sending to cloud storage and backend

--- src/components/homeComps/PostUpdate.jsx
+++ src/components/homeComps/PostUpdate.jsx
@@ -3,74 +3,112 @@ import { FaBell, FaCamera, FaCheck, FaCheckCircle, FaPen } from 'react-icons/fa'
 import './postUpdate.css'
 import { makePost } from '../../redux/posts/postSlice'
 import { useDispatch, useSelector } from 'react-redux'
+import Resizer from 'react-image-file-resizer'
+import axios from 'axios'
 
 const PostUpdate = () => {
-    const [postActive, setPostActive] = useState(false)
-    const { currentUser, userDetails } = useSelector((state) => state.user)
-    const dispatch = useDispatch()
+  const [postActive, setPostActive] = useState(false)
+  const { currentUser, userDetails } = useSelector((state) => state.user)
+  const dispatch = useDispatch()
+  const [text, setText] = useState('')
+  const [selectedImage, setSelectedImage] = useState(null);
+  const textareaRef = useRef(null);
+  const [progress, setProgress] = useState(false)
 
-    const textareaRef = useRef(null);
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (textareaRef.current && !textareaRef.current.contains(event.target)) {
+        setPostActive(false);
+      }
+    };
+    document.addEventListener('click', handleClickOutside);
 
-    useEffect(() => {
-        const handleClickOutside = (event) => {
-            if (textareaRef.current && !textareaRef.current.contains(event.target)) {
-                setPostActive(false);
-            }
-        };
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, []);
 
-        document.addEventListener('click', handleClickOutside);
+  const submit = async (e) => {
+    setProgress(true)
+    e.preventDefault()
+    const file = selectedImage;
+    const uploadPreset = process.env.CLOUDINARY_UPLOAD_PRESET
+    const cloud_name = process.env.CLOUDINARY_CLOUD_NAME
 
-        return () => {
-            document.removeEventListener('click', handleClickOutside);
-        };
-    }, []);
+    const maxWidth = 800;
+    const maxHeight = 800;
+    const quality = 75;
+    const fileType = 'image/jpeg';
 
-    const [text, setText] = useState('')
+    Resizer.imageFileResizer(
+      file,
+      maxWidth,
+      maxHeight,
+      fileType,
+      quality,
+      0,
+      async (resizedImage) => {
+        const formData = new FormData();
+        formData.append('file', resizedImage);
+        formData.append('upload_preset', uploadPreset)
 
-    const submit = (e) => {
-        e.preventDefault()
-        makePost({ text, TOKEN: currentUser?.data?.token }, dispatch)
-    }
+        try {
+          const postCloudinary = await axios.post(`https://api.cloudinary.com/v1_1/${cloud_name}/image/upload`, formData)
+          const imageUrl = postCloudinary.data.secure_url;
+          if (imageUrl) {
+            await makePost({ text, imageUrl, TOKEN: currentUser?.data?.token }, dispatch)
+          } else {
+            await makePost({ text, TOKEN: currentUser?.data?.token }, dispatch)
+          }
+          setProgress(false)
+        } catch (error) {
+          console.error('Error uploading image to Cloudinary:', error);
+        }
+      },
+      'base64'
+    );
+  }
 
-    return (
-        <div className='postUpdateDiv' >
-            <div className='postUpdateDivSm' >
-                <div className='postUpdateTabDiv' >
-                    <div className='postUpdateTab' >
-                        <FaPen />
-                        <p className='tabText' >Post</p>
-                    </div>
-                    <div className='postUpdateTab2' >
-                    </div>
-                </div>
-                <div className='postUpdateFormDiv' >
-                    <img className='postUpdateFormImage' src={userDetails?.image} alt="" />
-                    <form className='width100' >
-                        <textarea onChange={(e) => setText(e.target.value)} ref={textareaRef} onClick={() => setPostActive(true)} className='textarea' placeholder='Post something about you...' rows='7' />
-                    </form>
-                </div>
-                <div className='postUpdateBottomNavDiv padding1rem' >
-                    <div className='postUpdateBottomNav' >
-                        <FaCamera />
-                        <p className='font12' >media</p>
-                    </div>
-                </div>
-                {
-                    postActive &&
-                    <>
-                        <div className='flex align_center gap1rem padding1rem postActivityDiv'>
-                            <FaCheckCircle className='postActivityIcon' />
-                            <FaBell className='postActivityIcon' />
-                            <p className='white'>Activity Feed</p>
-                        </div>
-                        <div className='postUpdateButtonNavDiv' >
-                            <button onClick={submit} className='pointer postUpdateButton text_center' >Post</button>
-                        </div>
-                    </>
-                }
-            </div>
+  return (
+    <div className='postUpdateDiv' >
+      <div className='postUpdateDivSm' >
+        <div className='postUpdateTabDiv' >
+          <div className='postUpdateTab' >
+            <FaPen />
+            <p className='tabText' >Post</p>
+          </div>
+          <div className='postUpdateTab2' >
+          </div>
         </div>
-    )
+        <div className='postUpdateFormDiv' >
+          <img className='postUpdateFormImage' src={userDetails?.image} alt="" />
+          <form className='width100' >
+            <textarea onChange={(e) => setText(e.target.value)} ref={textareaRef} onClick={() => setPostActive(true)} className='textarea' placeholder='Post something about you...' rows='7' />
+          </form>
+        </div>
+        <div className='postUpdateBottomNavDiv padding1rem' >
+          <input style={{ display: 'none' }} type="file" id='image' accept='image/*' onChange={(e) => setSelectedImage(e.target.files[0])} />
+          <label className='postUpdateBottomNav pointer' htmlFor="image">
+            <FaCamera />
+            <p className='font12' >media</p>
+          </label>
+        </div>
+        {
+          postActive &&
+          <>
+            <div className='flex align_center gap1rem padding1rem postActivityDiv'>
+              <FaCheckCircle className='postActivityIcon' />
+              <FaBell className='postActivityIcon' />
+              <p className='white'>Activity Feed</p>
+            </div>
+            <div className='postUpdateButtonNavDiv' >
+              <button onClick={submit} className='pointer postUpdateButton text_center' >Post</button>
+            </div>
+          </>
+        }
+      </div>
+    </div>
+  )
 }
 
 export default PostUpdate
--- src/redux/posts/postSlice.js
+++ src/redux/posts/postSlice.js
@@ -2,151 +2,158 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
 export const getPosts = createAsyncThunk('posts/getPosts', async () => {
-    const res = await fetch('http://localhost:4000/api/v1/posts');
-    const data = await res.json()
-    return data
+  const res = await fetch('http://localhost:4000/api/v1/posts');
+  const data = await res.json()
+  return data
 })
 
 const BASE_URL = 'http://localhost:4000/api/v1'
 
 export const getPostsForAuthUser = createAsyncThunk('posts/getPostsForAuthUser', async ({ TOKEN }) => {
-    const userRequest = axios.create({
-        baseURL: BASE_URL,
-        headers: { 'Authorization': `Bearer ${TOKEN}` }
-    })
-    const res = await userRequest.get('all/posts')
-    delete res.headers
-    return res.data
+  const userRequest = axios.create({
+    baseURL: BASE_URL,
+    headers: { 'Authorization': `Bearer ${TOKEN}` }
+  })
+  const res = await userRequest.get('all/posts')
+  delete res.headers
+  return res.data
 })
 
 export const getPost = createAsyncThunk('posts/getOnePost', async (id) => {
-    const res = await fetch(`http://localhost:4000/api/v1/posts/${id}`);
-    const data = await res.json()
-    return data
+  const res = await fetch(`http://localhost:4000/api/v1/posts/${id}`);
+  const data = await res.json()
+  return data
 })
 
 export const getOnePostForAuthUser = createAsyncThunk('posts/getOnePostsForAuthUser', async ({ id, TOKEN }) => {
-    const userRequest = axios.create({
-        baseURL: BASE_URL,
-        headers: { 'Authorization': `Bearer ${TOKEN}` }
-    })
-    const res = await userRequest.get(`auth/post/${id}`)
-    delete res.headers
-    return res.data
+  const userRequest = axios.create({
+    baseURL: BASE_URL,
+    headers: { 'Authorization': `Bearer ${TOKEN}` }
+  })
+  const res = await userRequest.get(`auth/post/${id}`)
+  delete res.headers
+  return res.data
 })
 
-export const makePost = async ({ text, TOKEN }, dispatch) => {
-    try {
+export const makePost = async ({ text, imageUrl, TOKEN }, dispatch) => {
+  try {
 
-        const userRequest = axios.create({
-            baseURL: BASE_URL,
-            headers: { 'Authorization': `Bearer ${TOKEN}` }
-        })
-        await userRequest.post('posts', {
-            text: text
-        })
-    } catch (error) {
-        delete error.response
-        dispatch(getErrorStatus({
-            error: {
-                message: error.message,
-                code: error.code
-            }
-        }))
+    const userRequest = axios.create({
+      baseURL: BASE_URL,
+      headers: { 'Authorization': `Bearer ${TOKEN}` }
+    })
+    if (imageUrl) {
+      await userRequest.post('posts', {
+        text: text,
+        image: imageUrl
+      })
+    } else {
+      await userRequest.post('posts', {
+        text: text,
+      })
     }
+  } catch (error) {
+    delete error.response
+    dispatch(getErrorStatus({
+      error: {
+        message: error.message,
+        code: error.code
+      }
+    }))
+  }
 }
 
 const initialState = {
-    pending: null,
-    allPosts: [],
-    allPostsForAuthUser: [],
-    onePost: {},
-    error: null
+  pending: null,
+  allPosts: [],
+  allPostsForAuthUser: [],
+  onePost: {},
+  error: null
 }
 
 const getPostsSlice = createSlice({
-    name: 'allPosts',
-    initialState: initialState,
+  name: 'allPosts',
+  initialState: initialState,
 
-    reducers: {
-        getLikedStatus: (state, action) => {
-            const isLiked = action.payload.liked;
-            const count = action.payload.count
-            const id = action.payload.postId
-            const post = state.allPostsForAuthUser.find((post) => post.id === id);
-            if (post) {
-                post.liked = isLiked;
-                post.likes_count = count;
-            }
-        },
-        getErrorStatus: (state, action) => {
-            state.error = action.payload.error
-        },
-        resetState: () => initialState
+  reducers: {
+    getLikedStatus: (state, action) => {
+      const isLiked = action.payload.liked;
+      const count = action.payload.count
+      const id = action.payload.postId
+      const post = state.allPostsForAuthUser.find((post) => post.id === id);
+      if (post) {
+        post.liked = isLiked;
+        post.likes_count = count;
+      }
     },
-    extraReducers(reducers) {
-        reducers
-            .addCase(getPosts.pending, (state, action) => {
-                const isFulfilled = state;
-                isFulfilled.pending = 'pending no auth user';
-            })
-            .addCase(getPosts.fulfilled, (state, action) => {
-                const isFulfilled = state;
-                isFulfilled.pending = false;
-                isFulfilled.allPosts = action.payload.data
-            })
-            .addCase(getPosts.rejected, (state, action) => {
-                const isFulfilled = state;
-                isFulfilled.pending = false;
-                isFulfilled.allPosts = []
-                isFulfilled.error = action.error
-            })
-            .addCase(getPostsForAuthUser.pending, (state, action) => {
-                const isFulfilled = state;
-                isFulfilled.pending = 'pending and auth user';
-            })
-            .addCase(getPostsForAuthUser.fulfilled, (state, action) => {
-                const isFulfilled = state;
-                isFulfilled.pending = false;
-                isFulfilled.allPostsForAuthUser = action.payload.data
-            })
-            .addCase(getPostsForAuthUser.rejected, (state, action) => {
-                const isFulfilled = state;
-                isFulfilled.pending = false;
-                isFulfilled.allPostsForAuthUser = []
-                isFulfilled.error = action.error
-            })
-            .addCase(getPost.pending, (state, action) => {
-                const isFulfilled = state;
-                isFulfilled.pending = true;
-            })
-            .addCase(getPost.fulfilled, (state, action) => {
-                const isFulfilled = state;
-                isFulfilled.pending = false;
-                isFulfilled.onePost = action.payload.data
-            })
-            .addCase(getPost.rejected, (state, action) => {
-                const isFulfilled = state;
-                isFulfilled.pending = false;
-                isFulfilled.onePost = {};
-                isFulfilled.error = action.error
-            })
-            .addCase(getOnePostForAuthUser.pending, (state, action) => {
-                const isFulfilled = state;
-                isFulfilled.pending = true;
-            })
-            .addCase(getOnePostForAuthUser.fulfilled, (state, action) => {
-                const isFulfilled = state;
-                isFulfilled.pending = false;
-                isFulfilled.onePost = action.payload.data
-            })
-            .addCase(getOnePostForAuthUser.rejected, (state, action) => {
-                const isFulfilled = state;
-                isFulfilled.pending = false;
-                isFulfilled.onePost = {};
-                isFulfilled.error = action.error
-            })
-    }
+    getErrorStatus: (state, action) => {
+      state.error = action.payload.error
+    },
+    resetState: () => initialState
+  },
+  extraReducers(reducers) {
+    reducers
+      .addCase(getPosts.pending, (state, action) => {
+        const isFulfilled = state;
+        isFulfilled.pending = 'pending no auth user';
+      })
+      .addCase(getPosts.fulfilled, (state, action) => {
+        const isFulfilled = state;
+        isFulfilled.pending = false;
+        isFulfilled.allPosts = action.payload.data
+      })
+      .addCase(getPosts.rejected, (state, action) => {
+        const isFulfilled = state;
+        isFulfilled.pending = false;
+        isFulfilled.allPosts = []
+        isFulfilled.error = action.error
+      })
+      .addCase(getPostsForAuthUser.pending, (state, action) => {
+        const isFulfilled = state;
+        isFulfilled.pending = 'pending and auth user';
+      })
+      .addCase(getPostsForAuthUser.fulfilled, (state, action) => {
+        const isFulfilled = state;
+        isFulfilled.pending = false;
+        isFulfilled.allPostsForAuthUser = action.payload.data
+      })
+      .addCase(getPostsForAuthUser.rejected, (state, action) => {
+        const isFulfilled = state;
+        isFulfilled.pending = false;
+        isFulfilled.allPostsForAuthUser = []
+        isFulfilled.error = action.error
+      })
+      .addCase(getPost.pending, (state, action) => {
+        const isFulfilled = state;
+        isFulfilled.pending = true;
+      })
+      .addCase(getPost.fulfilled, (state, action) => {
+        const isFulfilled = state;
+        isFulfilled.pending = false;
+        isFulfilled.onePost = action.payload.data
+      })
+      .addCase(getPost.rejected, (state, action) => {
+        const isFulfilled = state;
+        isFulfilled.pending = false;
+        isFulfilled.onePost = {};
+        isFulfilled.error = action.error
+      })
+      .addCase(getOnePostForAuthUser.pending, (state, action) => {
+        const isFulfilled = state;
+        isFulfilled.pending = true;
+      })
+      .addCase(getOnePostForAuthUser.fulfilled, (state, action) => {
+        const isFulfilled = state;
+        isFulfilled.pending = false;
+        isFulfilled.onePost = action.payload.data
+      })
+      .addCase(getOnePostForAuthUser.rejected, (state, action) => {
+        const isFulfilled = state;
+        isFulfilled.pending = false;
+        isFulfilled.onePost = {};
+        isFulfilled.error = action.error
+      })
+  }
 })
 
 export const { getLikedStatus, getErrorStatus, resetState } = getPostsSlice.actions;
