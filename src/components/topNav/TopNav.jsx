e763c8bd494822972a5ffd5c47a10c02e33793bd
Kenny Egun <kennyegun240@gmail.com>
2023-07-19 14:00:18 +0100

    Add links to other components

--- src/components/topNav/TopNav.jsx
+++ src/components/topNav/TopNav.jsx
@@ -1,22 +1,26 @@
 import React from 'react'
 import { FaBell, FaHeart, FaInbox, FaMailchimp } from 'react-icons/fa'
-import { BiMessage } from 'react-icons/bi'
+import { BiHomeHeart, BiMessage, BiShoppingBag } from 'react-icons/bi'
 import { AiOutlineMail, AiOutlineHeart } from 'react-icons/ai'
-import { BsBell } from 'react-icons/bs'
+import { BsBell, BsTwitter } from 'react-icons/bs'
 import './topnav.css'
 import noImage from '../../images/noImage.png'
 import { Link } from 'react-router-dom'
 import { useSelector } from 'react-redux'
+import { SlBriefcase } from 'react-icons/sl'
 
 const TopNav = () => {
     const { currentUser, userDetails } = useSelector((state) => state.user)
     return (
         <div className='topNavMainDiv mainBackground justify_between align_center flex'>
             <div className='flex align_center gap15rem' >
+                <Link to='/'><BiHomeHeart className='icons' /></Link>
                 <AiOutlineHeart className='icons' />
                 <BsBell className='icons' />
                 <AiOutlineMail className='icons' />
                 <BiMessage className='icons' />
+                <Link to='/store'><BiShoppingBag className='icons' /></Link>
+                <Link to='/jobs'><SlBriefcase className='icons' /></Link>
             </div>
             <div className='flex align_center gap15rem topNavSearchDiv' >
                 <input className='width100 padding05rem topNavSearchInput' type="text" placeholder='Search.....' />
