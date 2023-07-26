a69c7b389df2ce084d66f63056992275b2645fa5
Kenny Egun <kennyegun240@gmail.com>
2023-07-26 16:01:45 +0100

    Notification compoents created and styled

---
+++ src/pages/notification/Notification.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Notify from '../../components/notify/Notify'
+
+const Notification = () => {
+    return (
+        <div className='flex justify_center align_center' style={{ background: '#2F3B50', height: '90vh' }}>
+            <Notify />
+        </div>
+    )
+}
+
+export default Notification
