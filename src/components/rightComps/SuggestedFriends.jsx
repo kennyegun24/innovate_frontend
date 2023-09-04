d13f059924818861381b45f98e31172af752a3cc
Kenny Egun <kennyegun240@gmail.com>
2023-09-04 23:01:53 +0100

    Consumed recommended users and populated it

--- src/components/rightComps/SuggestedFriends.jsx
+++ src/components/rightComps/SuggestedFriends.jsx
@@ -1,58 +1,45 @@
-import React from 'react'
-import vic from '../../images/vic.jpg'
-import image1 from '../../images/kenny.png'
-import image2 from '../../images/noImage.png'
-import image3 from '../../images/screen-0.jpg'
-import image4 from '../../images/screen-3.jpg'
-import { FaBookmark, FaTag, FaUserPlus } from 'react-icons/fa'
-import './style.css'
+import React from "react";
+import { FaUserPlus } from "react-icons/fa";
+import "./style.css";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { fetchRecommendedUsers } from "../../redux/auth_redux/user_recommend/UserRecommend";
+import { Link } from "react-router-dom";
 
 const SuggestedFriends = () => {
-    const data = [
-        {
-            image: vic,
-            name: 'Jon Doe',
-            profession: 'MERN Developer'
-        },
-        {
-            image: image1,
-            name: 'Peter Pete',
-            profession: 'Backend Developer'
-        },
-        {
-            image: image2,
-            name: 'Jon Snow',
-            profession: 'FullStack Developer'
-        },
-        {
-            image: image3,
-            name: 'Sapa Bien',
-            profession: 'Java Developer'
-        },
-        {
-            image: image4,
-            name: 'Sam Lateef',
-            profession: 'Ruby on Rails Developer'
-        },
-    ]
-    return (
-        <>
-            {
-                data.map((each) => (
-                    <div className='white flex align_center justify_between suggestedFriendsPagesMainDiv' style={{}}>
-                        <div className='flex align_center gap05rem'>
-                            <img src={each.image} className='postUpdateFormImage' alt="" />
-                            <div>
-                                <p className='font14 weight700'>{each.name}</p>
-                                <p className='font12 opacity05'>{each.profession}</p>
-                            </div>
-                        </div>
-                        <FaUserPlus />
-                    </div>
-                ))
-            }
-        </>
-    )
+  const dispatch = useDispatch();
+  const { currentUser } = useSelector((state) => state.user);
+  const { recommendedUsers } = useSelector((state) => state.recommended_users);
+
+  useEffect(() => {
+    dispatch(
+      fetchRecommendedUsers({
+        TOKEN: currentUser?.data?.token,
+      })
+    );
+  }, [dispatch, currentUser?.data?.token]);
+  return (
+    <>
+      {recommendedUsers.map((each) => (
+        <div
+          className="white flex align_center justify_between suggestedFriendsPagesMainDiv"
+          style={{}}
+        >
+          <Link
+            to={`/user/${each.name}/${each.id}/profile`}
+            className="flex align_center gap05rem white decoration_none"
+          >
+            <img src={each.image} className="postUpdateFormImage" alt="" />
+            <div>
+              <p className="font14 weight700">{each.name}</p>
+              <p className="font12 opacity05">{each.profession}</p>
+            </div>
+          </Link>
+          <FaUserPlus />
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default SuggestedFriends;
