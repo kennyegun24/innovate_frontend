070ff6df015f27c643b4974d7d2d4f830560296e
Kenny Egun <kennyegun240@gmail.com>
2023-09-04 22:53:18 +0100

    Imported css file and reduced space indentations

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,53 +1,65 @@
-import React, { useState } from 'react'
-import vic from '../../images/noImage.png'
-import { useDispatch } from 'react-redux'
-import { loginFailure } from '../../redux/auth_redux/user/userSlice'
-import { login } from '../../redux/apiCalls'
+import React, { useState } from "react";
+import vic from "../../images/noImage.png";
+import { useDispatch } from "react-redux";
+import { loginFailure } from "../../redux/auth_redux/user/userSlice";
+import { login } from "../../redux/apiCalls";
+import "./auth.css";
 
 const Login = () => {
+  const dispatch = useDispatch();
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
 
-    const dispatch = useDispatch()
-    const [email, setEmail] = useState(null)
-    const [password, setPassword] = useState(null)
-
-    const loginUser = async (e) => {
-        e.preventDefault()
-        try {
-            await login(dispatch, {
-                email,
-                password
-            })
-        } catch (error) {
-            dispatch(loginFailure)
-        }
+  const loginUser = async (e) => {
+    e.preventDefault();
+    try {
+      await login(dispatch, {
+        email,
+        password,
+      });
+    } catch (error) {
+      dispatch(loginFailure);
     }
-    return (
-        <div className='authDiv' >
-            <div className='authDivSm'>
-                <form className='formDiv'>
-                    <div>
-                        <img className='img' src={vic} alt="" />
-                    </div>
-                    <div className="formInputDiv">
-                        <input onChange={(e) => setEmail(e.target.value)} className='formInputs' type="text" placeholder='Enter your email address' />
-                        <input onChange={(e) => setPassword(e.target.value)} className='formInputs' type="text" placeholder='Choose your password' />
-                        <button className='formInputBtn' onClick={loginUser}>Login</button>
-                        <p style={{ color: '#fff' }}>Forot your password?</p>
-                    </div>
-                </form>
-                <div className='centerCircleDiv'>
-                    <div className='centerCircle' />
-                </div>
-                <div className='authHeaderDiv'>
-                    <h1 className='authHeader'>
-                        Resume from
-                        <br />
-                        where you stopped
-                    </h1>
-                </div>
-            </div>
+  };
+  return (
+    <div className="authDiv">
+      <div className="authDivSm">
+        <form className="formDiv">
+          <div>
+            <img className="img" src={vic} alt="" />
+          </div>
+          <div className="formInputDiv">
+            <input
+              onChange={(e) => setEmail(e.target.value)}
+              className="formInputs"
+              type="text"
+              placeholder="Enter your email address"
+            />
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              className="formInputs"
+              type="text"
+              placeholder="Choose your password"
+            />
+            <button className="formInputBtn" onClick={loginUser}>
+              Login
+            </button>
+            <p style={{ color: "#fff" }}>Forot your password?</p>
+          </div>
+        </form>
+        <div className="centerCircleDiv">
+          <div className="centerCircle" />
         </div>
-    )
-}
+        <div className="authHeaderDiv">
+          <h1 className="authHeader">
+            Resume from
+            <br />
+            where you stopped
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
